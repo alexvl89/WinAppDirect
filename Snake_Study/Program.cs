fc975046aefd9c43941f8b4ea1f43eadc0c10a7a
Alexvl89 <alexvl89.ru@gmail.com>
2016-11-02 20:09:40 +0300

    Добавление класса Point

--- Snake_Study/Program.cs
+++ Snake_Study/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
 
 namespace Snake_Study
 {
@@ -10,17 +11,21 @@ namespace Snake_Study
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
-            int y1 = 1;
-            char sym1 = "*";
 
-            Draw(x1, y1, sym1);
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 1;
+            p1.sym = '*';
 
-            int x2 = 4;
-            int y2 = 5;
-            char sym2 = "#";
+            Draw(p1.x, p1.y, p1.sym);
 
-            Draw(x2, y2, sym2);
+
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.sym = '#';
+
+            Draw(p2.x, p2.y, p2.sym);
 
             Console.ReadLine();
         }
