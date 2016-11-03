bba77e8118c35ac6f692c0cc9e2a69f74b2c71aa
Alexvl89 <alexvl89.ru@gmail.com>
2016-11-03 20:52:51 +0300

    Добавление класса HorizontalLine

--- Snake_Study/Program.cs
+++ Snake_Study/Program.cs
@@ -14,36 +14,14 @@ namespace Snake_Study
 
             Point p1 = new Point(1, 1, '*');
             p1.Draw();
-            
+
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
+            HorizontalLine line = new HorizontalLine(5,10,8,'*');
+            line.Drow();
 
-            List<int> numList = new List<int>();
-            numList.Add(1);
-            numList.Add(2);
-            numList.Add(3);
-            numList.Add(4);
-
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-            int w = numList[3];
-
-            Console.WriteLine();
-
-            foreach (int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-
-            numList.RemoveAt(0);
-
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
-
+            
             Console.ReadLine();
         }
 
