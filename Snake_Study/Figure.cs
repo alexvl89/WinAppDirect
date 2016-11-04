1d0a732e33f1e901937b89186affef98440d90e4
Alexvl89 <alexvl89.ru@gmail.com>
2016-11-04 17:50:36 +0300

    Изменение Snake и Direction

---
+++ Snake_Study/Figure.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_Study
+{
+    class Figure
+    {
+        protected List<Point> pList;
+
+        public void Drow()
+        {
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
+
+        }
+
+
+
+    }
+}
